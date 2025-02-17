3e9e723652a2fba1693e89355847902215e44486
geremiasrds <geremiasds00@gmail.com>
2025-02-17 16:26:42 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -1,140 +1,48 @@
-// const pegarInput = document.getElementById('inputs')
-// const oqueDigitou = pegarInput.value
-// console.log(oqueDigitou)
-
-
-// const input = document.getElementById('inputs');
-
-// input.addEventListener('input', (e) => {
-//   const valorDigitado = e.target.value;
-// });
-
-
-
-
-
-// const form = document.getElementsByClassName('formulario')
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   const input = document.getElementById('meu-input');
-//   const valorDigitado = input.value;
-//   console.log(valorDigitado)
-//   // Enviar os dados para o servidor
-// });
-
-
-
-
-
-
-
-
-// const inputs = document.getElementById('meu-input');
-
-// inputs.addEventListener('change', (e) => {
-//   const valorDigitado = e.target.value;
-
-
-
-//  console.log(valorDigitado)
-
-
-// });
-
-
-
-
-// const input = document.getElementById('meu-input');
-// const botao = document.getElementById('enviar');
-
-// botao.addEventListener('click', (e) => {
-//   const valorDigitado = input.value;
-//   const numero = '+5591984572107';
-// //   const link = `https://wa.me/${559199069633}?text=${encodeURIComponent(valorDigitado)}`;
-// //   window.open(link, '_blank');
-// });
-
-
-
-
-
-
-
-
-
-// function enviar() {
-//     const inputs = document.getElementById('meu-input');
-//     inputs.addEventListener('change', (e) => {
-//         const valorDigitado = e.target.value
-//         console.log(valorDigitado)
-
-//         //     // const url = `https://wa.me/${91984572107}?text=${encodeURIComponent(mensagem)}`;
-//         //     // window.open(url, '_blank');
-//     });
-
-// }
-
-// const botao = document.getElementById('enviar');
-// console.log(botao)
-
-
-
-
-
-
-
-
-
 const botao = document.getElementById('enviar');
 
 botao.addEventListener('click', (e) => {
     const input = document.getElementById('meu-input')
-    const inputNuber = document.getElementById ('meu-input-nuber')
+    const inputNuber = document.getElementById('meu-input-nuber')
     const mensagem = input.value
     const mensagem2 = inputNuber.value
- 
+
     const numero = '+559199069633'; // número de telefone do destinatário
 
-      const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem, mensagem2)}`;
-      window.open(url, '_blank');
+    const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem, mensagem2)}`;
+    window.open(url, '_blank');
 });
 
 
 
-// // Salvar o que foi digitado no input
-// const input = document.getElementById('meu-input');
-// const valorDigitado = input.value;
-// localStorage.setItem('meu-dado', valorDigitado);
 
-// // Recuperar o que foi digitado
-// const dadoSalvo = localStorage.getItem('meu-dado');
-// console.log(dadoSalvo);
+const classVazia = document.querySelector('.mostrar-clicar')
+function btnVisita() {
+    const pegar1 = classVazia.childNodes[1]
+    const pegar2 = classVazia.childNodes[3]
+    const pegar3 = classVazia.childNodes[5]
+    pegar1.style.display = 'block'
+    pegar2.style.display = 'none'
+    pegar3.style.display = 'none'
 
+}
+function btnIstalacao() {
+    const pegar1 = classVazia.childNodes[1]
+    const pegar2 = classVazia.childNodes[3]
+    const pegar3 = classVazia.childNodes[5]
+    pegar1.style.display = 'none'
+    pegar2.style.display = 'block'
+    pegar3.style.display = 'none'
 
+}
 
-// // Salvar o que foi digitado no input
-// const input = document.getElementById('meu-input');
-// const valorDigitado = input.value;
-// document.cookie = `meu-dado=${valorDigitado}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+function btnLimpeza() {
+    const pegar1 = classVazia.childNodes[1]
+    const pegar2 = classVazia.childNodes[3]
+    const pegar3 = classVazia.childNodes[5]
+    pegar1.style.display = 'none'
+    pegar2.style.display = 'none'
+    pegar3.style.display = 'block'
 
-// // Recuperar o que foi digitado
-// const cookie = document.cookie;
-// const dadoSalvo = cookie.split('=')[1];
-// console.log(dadoSalvo);
-
-
-// Salvar o que foi digitado no input
-// const input = document.getElementById('meu-input');
-// const valorDigitado = input.value;
-// fetch('index.html?nome=&tel=5591986128843', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify({ dado: valorDigitado }),
-// })
-// .then((res) => res.json())
-// .then((data) => console.log(data))
-// .catch((err) => console.error(err));
+}
 
 
